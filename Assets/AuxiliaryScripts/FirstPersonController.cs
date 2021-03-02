43532abfc89acca10139a0e327501aff318d3e7e
Christian Schöffmann <christian.stetco@aau.at>
2021-03-02 09:42:09 +0100

    added fps controller

---
+++ Assets/AuxiliaryScripts/FirstPersonController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    //private float minX = -60.0f;
+    //private float maxX = 60.0f;
+    public Animator animator; 
+    public float sensitivityX = 3.0f;
+    public float sensitivityFwd = 0.05f;
+    private float sensitivityY = 3.0f;
+
+    public GameObject cam; 
+
+    private float rotationY = 0.0f; 
+    private float rotationX = 0.0f;
+    private float posFwd = 0.0f;
+    private float posSideways = 0.0f;
+
+    private Vector3 rotVector = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 posVector = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = cam.GetComponent<Animator>();
+        Debug.Log("First-Person Controller started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY += Input.GetAxis("Horizontal") * sensitivityY;
+        rotationX += Input.GetAxis("Vertical") * sensitivityX;
+
+       
+        if (Input.GetAxis("MoveForward") < 0)
+        {
+            posFwd += Input.GetAxis("MoveForward") * sensitivityFwd;
+            animator.Play("Base_Layer.WalkFwd");
+        }
+        else if (Input.GetAxis("MoveForward") > 0)
+        {
+            posFwd += Input.GetAxis("MoveForward") * sensitivityFwd;
+            animator.Play("Base_Layer.WalkBwd");
+        }
+        else
+        {
+            posFwd = 0.0f;
+            animator.Play("Base_Layer.Idle");
+        }    
+
+
+        rotVector.x = -rotationX;
+        rotVector.y = rotationY;
+        posVector.z = -posFwd; 
+
+        cam.transform.localEulerAngles = rotVector;
+        cam.transform.Translate(posVector * Time.deltaTime);
+    }
+}
+
---
+++ Assets/AuxiliaryScripts/HumanController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    //private float minX = -60.0f;
+    //private float maxX = 60.0f;
+    public Animator animator;
+    public float velocity = 0.05f;
+    public float angVelocity = 3.0f;
+
+    public GameObject cam;
+
+    private float rotationY = 0.0f;
+    private float posFwd = 0.0f;
+    private float posSideways = 0.0f;
+
+    private Vector3 rotVector = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 posVector = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = cam.GetComponent<Animator>();
+        Debug.Log("First-Person Controller started");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotationY += Input.GetAxis("Horizontal") * angVelocity;
+
+        if (Input.GetKey("w"))
+        {
+            posFwd -= (velocity * Time.fixedDeltaTime);
+            animator.Play("Base_Layer.WalkFwd");
+            animator.speed = velocity;
+        }
+        else if (Input.GetKey("s"))
+        {
+            posFwd += (velocity * Time.fixedDeltaTime);
+            animator.Play("Base_Layer.WalkBwd");
+            animator.speed = velocity;
+        }
+        else
+        {
+            posFwd = 0.0f;
+            animator.Play("Base_Layer.Idle");
+        }
+  
+
+        rotVector.y = rotationY;
+        posVector.z = -posFwd;
+
+        cam.transform.localEulerAngles = rotVector;
+        cam.transform.Translate(posVector * Time.fixedDeltaTime);
+    }
+}
